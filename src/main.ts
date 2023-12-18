8c8494bec5fa64f57e32fddf33b61b6f85045cbc
Alfatta Rezqa <alfatta@mariworks.id>
2023-12-18 16:57:56 +0700

    setup clean structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from "vue";
-import "@/style.css";
-import App from "@/App.vue";
+import app from "@/core/app";
 
-createApp(App).mount("#app");
+import "@/assets/css/main.css";
+
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       imports: ["vue"],
     }),
     Component({
-      dirs: ["./src/components"],
+      dirs: ["./src/views/components"],
     }),
   ],
   resolve: {
