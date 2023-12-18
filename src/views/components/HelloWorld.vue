ce2e97d471bf6c3fa90f363600f8c40731a2a753
Alfatta Rezqa <alfatta@mariworks.id>
2023-12-18 17:54:17 +0700

    setup tailwind

--- src/views/components/HelloWorld.vue
+++ src/views/components/HelloWorld.vue
@@ -11,7 +11,10 @@ const { t } = useI18n()
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="increment">count is {{ count }}</button>
+    <button type="button" @click="increment"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      count is {{ count }}
+    </button>
     <p>
       Edit
       <code>views/components/HelloWorld.vue</code> to test HMR
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="flex justify-center">
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo w-32 p-5" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/img/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="@/assets/img/vue.svg" class="logo vue w-32 p-5" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -12,8 +12,6 @@
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
