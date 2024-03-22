7a3f45d51e8e0e1a5f70e93c986aee050d554f5e
Alfatta Rezqa <alfatta@mariworks.id>
2024-03-22 14:20:27 +0700

    add pinia persisted state

--- src/core/pinia.ts
+++ src/core/pinia.ts
@@ -1,3 +1,7 @@
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-export default createPinia();
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+export default pinia;
